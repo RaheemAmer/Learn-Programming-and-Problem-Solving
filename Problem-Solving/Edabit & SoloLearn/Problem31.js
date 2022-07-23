dc953895d99c234e4a819a5f3edafa2887091b9d
raheemamer <raheem.amer22@gmail.com>
2022-07-23 17:56:47 +0200

    Solved some problems

---
+++ Problem-Solving/Edabit & SoloLearn/Problem31.js
@@ -0,0 +1,6 @@
+//Basketball Points
+//You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored, find the final points for the team and return that value.
+
+function points(twoPointers, threePointers) {
+    return (twoPointers * 2) + (threePointers * 3);
+}
