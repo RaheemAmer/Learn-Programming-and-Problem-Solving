f5cdef8d9617e4703c2bb1aa85ddf5834bf0b0ea
raheemamer <raheem.amer22@gmail.com>
2022-08-16 22:50:49 +0200

    Solved Power of two

---
+++ Problem-Solving/Leetcode/231. Power of Two.js
@@ -0,0 +1,20 @@
+// 231. Power of Two
+// Easy
+
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+
+var isPowerOfTwo = function (n) {
+    if (n <= 0) {
+        return false;
+    }
+    if (n == 1) {
+        return true;
+    }
+    if (n % 2 != 0) {
+        return false;
+    }
+    return isPowerOfTwo(n / 2);
+};
